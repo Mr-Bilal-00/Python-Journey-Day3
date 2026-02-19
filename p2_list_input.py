4edb5eb247b656f8074355a744c6d9e455adb6af
Mr-Bilal-00 <mrbilalakram0@gmail.com>
2026-02-19 08:25:14 +0500

    student_marks.py
    Program 2 (5 marks, calculate average)

---
+++ p2_list_input.py
@@ -0,0 +1,8 @@
+marks=[]
+marks.append(int(input("Enter 1st mark: ")))
+marks.append(int(input("Enter 2nd mark: ")))
+marks.append(int(input("Enter 3rd mark: ")))
+marks.append(int(input("Enter 4th mark: ")))
+marks.append(int(input("Enter 5th mark: ")))
+average=sum(marks)/5
+print("Avearge Of No is :",average)
